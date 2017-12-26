17b880630eff4d7e7382e3bfc7dd48e8cfe8dd09
Florian Moser <products@famoser.ch>
2017-12-26 12:16:39 +0100

    upading bootstrap, crypto research

---
+++ Famoser.Bookmarked.Presentation.Webpage/Platform/DispatchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Famoser.Bookmarked.Business.Services.Interfaces;
+
+namespace Famoser.Bookmarked.Presentation.Webpage.Platform
+{
+    public class DispatchService : IDispatchService
+    {
+        public void CheckBeginInvokeOnUi(Action action)
+        {
+            action.Invoke();
+        }
+    }
+}
---
+++ Famoser.Bookmarked.Presentation.Webpage/Scripts/script.js
@@ -0,0 +1,12 @@
+﻿async function sha256(message) {
+    const msgBuffer = new TextEncoder('utf-8').encode(message);                     // encode as UTF-8
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);            // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                       // convert ArrayBuffer to Array
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join(''); // convert bytes to hex string
+    return hashHex;
+}
+
+function setPassword() {
+    const $val = $("#password").value;
+    sha256($val).then(hash => window.location=hash);
+}
