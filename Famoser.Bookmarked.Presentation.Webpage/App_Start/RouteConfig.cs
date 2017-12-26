4d587cc42c26293cee1ff510f02adb44b2602e63
Florian Moser <products@famoser.ch>
2017-12-26 12:24:56 +0100

    updating nuget packages

--- Famoser.Bookmarked.Presentation.Webpage/App_Start/RouteConfig.cs
+++ Famoser.Bookmarked.Presentation.Webpage/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace Famoser.Bookmarked.Presentation.Webpage
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "LoggedIn",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ Famoser.Bookmarked.Presentation.Webpage/Controllers/ViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Famoser.Bookmarked.Presentation.Webpage.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
